96f281baf88f450aeaf6e9933541ee75731d1c1c
unknown <alinabih323@gmail.com>
2023-02-20 14:41:37 +0200

    FCommit

---
+++ app/src/main/java/com/h/alrekhawi/lect13constraintlayoutexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.h.alrekhawi.lect13constraintlayoutexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
